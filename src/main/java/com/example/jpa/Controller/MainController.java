231f21d8d05f5354d022c8ecdc28d07332040b08
botkoda <botkoda@gmail.com>
2022-03-25 13:14:57 +0400

    add oauth2

--- src/main/java/com/example/jpa/Controller/MainController.java
+++ src/main/java/com/example/jpa/Controller/MainController.java
@@ -2,11 +2,15 @@ package com.example.jpa.Controller;
 
 import com.example.jpa.Entity.Person;
 import com.example.jpa.Repository.MainRepository;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
@@ -23,19 +27,28 @@ public class MainController {
     }
 
     @GetMapping("persons/by-city")
+    @Secured("ROLE_READ")
     public List<Person> getPersonByCityName(@RequestParam("city") String name) {
         return mainRepository.findByCityOfLiving(name);
     }
 
     @GetMapping("persons/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> getPersonByName(@RequestParam("age") int age) {
         return mainRepository.findByContactAgeLessThanOrderByContactAgeDesc(age);
     }
 
     @GetMapping("persons/by-name-surname")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public Optional<Person> getPerson(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         final Person entity = mainRepository.findByContactNameAndContactSurname(name, surname)
                 .orElseThrow(() -> new EntityNotFoundException("Entity " + name + " " + surname + " not found"));
         return Optional.ofNullable(entity);
+    }
+
+    @GetMapping("/hello")
+    @PostAuthorize("#username == authentication.principal.username")
+    public String hello(@RequestParam("username") String username) {
+        return username;
     }
 }
--- src/main/java/com/example/jpa/config/SecurityConfig.java
+++ src/main/java/com/example/jpa/config/SecurityConfig.java
@@ -2,24 +2,37 @@ package com.example.jpa.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws  Exception{
-        auth.inMemoryAuthentication().withUser("Ivan").password("{noop}1234").roles("NOT_ALL");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws  Exception{
-        http.formLogin()
+        auth.inMemoryAuthentication()
+                .withUser("roman")
+                .password("{noop}1234")
+                .roles("READ")
                 .and()
-                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
+                .withUser("ivan")
+                .password("{noop}1234")
+                .roles("WRITE")
                 .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .withUser("levitan")
+                .password("{noop}1234")
+                .roles("DELETE");
     }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws  Exception{
+//        http.formLogin()
+//                .and()
+//                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
+//                .and()
+//                .authorizeRequests().anyRequest().authenticated();
+//    }
 }
